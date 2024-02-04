7b54c937fbc9df1eed56edaed4229c20cd6e5834
hyori <gyfl5542@naver.com>
2024-02-04 23:58:39 +0900

    how to use jobparameters

--- src/main/java/org/study/springbatch/job/DeciderJobConfiguration.java
+++ src/main/java/org/study/springbatch/job/DeciderJobConfiguration.java
@@ -21,77 +21,77 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Configuration
 public class DeciderJobConfiguration {
-	@Bean
-	public Job decideJob(JobRepository jobRepository, JobExecutionDecider decider, Step startStep, Step evenStep, Step oddStep) {
-		return new JobBuilder("decideJob", jobRepository)
-			.start(startStep)
-			.next(decider) // 스탭의 exitcode 대신 flowExecutionStatus로 관리
-			.from(decider)
-				.on("ODD")
-				.to(oddStep)
-			.from(decider)
-				.on("EVEN")
-				.to(evenStep)
-			.end()
-			.build();
-	}
-
-
-	@Bean
-	public Step startStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-		return new StepBuilder("startStep", jobRepository)
-			.tasklet((contribution, chunkContext) -> {
-				log.info(">>>>> Start!");
-				return RepeatStatus.FINISHED;
-			}, transactionManager)
-			.build();
-	}
-
-	@Bean
-	public Step evenStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-		return new StepBuilder("evenStep", jobRepository)
-			.tasklet((contribution, chunkContext) -> {
-				log.info(">>>>> 짝수입니다.");
-				return RepeatStatus.FINISHED;
-			}, transactionManager)
-			.build();
-	}
-
-
-	@Bean
-	public Step oddStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-		return new StepBuilder("oddStep", jobRepository)
-			.tasklet((contribution, chunkContext) -> {
-				log.info(">>>>> 홀수입니다.");
-				return RepeatStatus.FINISHED;
-			}, transactionManager)
-			.build();
-	}
-
-
-	@Bean
-	public JobExecutionDecider decider(){
-		return new OddDecider();
-	}
-
-	public static class OddDecider implements JobExecutionDecider {
-		@Override
-		public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution){
-			Random rand = new Random();
-
-			int randomNumber = rand.nextInt(50) + 1;
-			log.info("랜덤숫자 : {}", randomNumber);
-
-
-			if(randomNumber % 2 == 0) {
-				// 스탭의 exitcode가 아니라 flowEwecutionStatus로 관리함.
-				return new FlowExecutionStatus("EVEN");
-			} else {
-				return new FlowExecutionStatus("ODD");
-			}
-		}
-	}
-
+	// @Bean
+	// public Job decideJob(JobRepository jobRepository, JobExecutionDecider decider, Step startStep, Step evenStep, Step oddStep) {
+	// 	return new JobBuilder("decideJob", jobRepository)
+	// 		.start(startStep)
+	// 		.next(decider) // 스탭의 exitcode 대신 flowExecutionStatus로 관리
+	// 		.from(decider)
+	// 			.on("ODD")
+	// 			.to(oddStep)
+	// 		.from(decider)
+	// 			.on("EVEN")
+	// 			.to(evenStep)
+	// 		.end()
+	// 		.build();
+	// }
+	//
+	//
+	// @Bean
+	// public Step startStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+	// 	return new StepBuilder("startStep", jobRepository)
+	// 		.tasklet((contribution, chunkContext) -> {
+	// 			log.info(">>>>> Start!");
+	// 			return RepeatStatus.FINISHED;
+	// 		}, transactionManager)
+	// 		.build();
+	// }
+	//
+	// @Bean
+	// public Step evenStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+	// 	return new StepBuilder("evenStep", jobRepository)
+	// 		.tasklet((contribution, chunkContext) -> {
+	// 			log.info(">>>>> 짝수입니다.");
+	// 			return RepeatStatus.FINISHED;
+	// 		}, transactionManager)
+	// 		.build();
+	// }
+	//
+	//
+	// @Bean
+	// public Step oddStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+	// 	return new StepBuilder("oddStep", jobRepository)
+	// 		.tasklet((contribution, chunkContext) -> {
+	// 			log.info(">>>>> 홀수입니다.");
+	// 			return RepeatStatus.FINISHED;
+	// 		}, transactionManager)
+	// 		.build();
+	// }
+	//
+	//
+	// @Bean
+	// public JobExecutionDecider decider(){
+	// 	return new OddDecider();
+	// }
+	//
+	// public static class OddDecider implements JobExecutionDecider {
+	// 	@Override
+	// 	public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution){
+	// 		Random rand = new Random();
+	//
+	// 		int randomNumber = rand.nextInt(50) + 1;
+	// 		log.info("랜덤숫자 : {}", randomNumber);
+	//
+	//
+	// 		if(randomNumber % 2 == 0) {
+	// 			// 스탭의 exitcode가 아니라 flowEwecutionStatus로 관리함.
+	// 			return new FlowExecutionStatus("EVEN");
+	// 		} else {
+	// 			return new FlowExecutionStatus("ODD");
+	// 		}
+	// 	}
+	// }
+	//
 
 
 
